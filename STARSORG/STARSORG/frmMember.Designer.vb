28dbcd62ad2ecf346227edde6e3dd0eca3567375
GoreDough <eblen001@fiu.edu>
2019-11-23 16:23:53 -0500

    1st commit: frmMembership
    1st Commit: Added a form for Membership frmMembership skeleton

---
+++ STARSORG/STARSORG/frmMember.Designer.vb
@@ -0,0 +1,377 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmMember
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.ToolStripSeparator10 = New System.Windows.Forms.ToolStripSeparator()
+        Me.tsbHelp = New System.Windows.Forms.ToolStripButton()
+        Me.ToolStripSeparator1 = New System.Windows.Forms.ToolStripSeparator()
+        Me.tsbLogOut = New System.Windows.Forms.ToolStripButton()
+        Me.ToolStripSeparator9 = New System.Windows.Forms.ToolStripSeparator()
+        Me.tsbTutor = New System.Windows.Forms.ToolStripButton()
+        Me.tsbSemester = New System.Windows.Forms.ToolStripButton()
+        Me.ToolStripSeparator2 = New System.Windows.Forms.ToolStripSeparator()
+        Me.tsbCourse = New System.Windows.Forms.ToolStripButton()
+        Me.ToolStripSeparator4 = New System.Windows.Forms.ToolStripSeparator()
+        Me.ToolStripSeparator11 = New System.Windows.Forms.ToolStripSeparator()
+        Me.tsbRSVP = New System.Windows.Forms.ToolStripButton()
+        Me.tsbEvent = New System.Windows.Forms.ToolStripButton()
+        Me.ToolStripSeparator5 = New System.Windows.Forms.ToolStripSeparator()
+        Me.tsbRole = New System.Windows.Forms.ToolStripButton()
+        Me.ToolStripSeparator8 = New System.Windows.Forms.ToolStripSeparator()
+        Me.tsbMember = New System.Windows.Forms.ToolStripButton()
+        Me.ToolStripSeparator7 = New System.Windows.Forms.ToolStripSeparator()
+        Me.tsbHome = New System.Windows.Forms.ToolStripButton()
+        Me.ToolStripSeparator6 = New System.Windows.Forms.ToolStripSeparator()
+        Me.ToolStrip1 = New System.Windows.Forms.ToolStrip()
+        Me.ToolStripSeparator3 = New System.Windows.Forms.ToolStripSeparator()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.lsvMembers = New System.Windows.Forms.ListView()
+        Me.lblAddMember = New System.Windows.Forms.Label()
+        Me.txtAddMember = New System.Windows.Forms.TextBox()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.lblModifyMember = New System.Windows.Forms.Label()
+        Me.txtSearchMember = New System.Windows.Forms.TextBox()
+        Me.lblSearchMember = New System.Windows.Forms.Label()
+        Me.ToolStrip1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'ToolStripSeparator10
+        '
+        Me.ToolStripSeparator10.Alignment = System.Windows.Forms.ToolStripItemAlignment.Right
+        Me.ToolStripSeparator10.AutoSize = False
+        Me.ToolStripSeparator10.Name = "ToolStripSeparator10"
+        Me.ToolStripSeparator10.Size = New System.Drawing.Size(10, 50)
+        '
+        'tsbHelp
+        '
+        Me.tsbHelp.Alignment = System.Windows.Forms.ToolStripItemAlignment.Right
+        Me.tsbHelp.AutoSize = False
+        Me.tsbHelp.BackgroundImage = Global.STARSORG.My.Resources.Resources.help
+        Me.tsbHelp.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom
+        Me.tsbHelp.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image
+        Me.tsbHelp.ImageTransparentColor = System.Drawing.Color.Magenta
+        Me.tsbHelp.Name = "tsbHelp"
+        Me.tsbHelp.Size = New System.Drawing.Size(48, 48)
+        Me.tsbHelp.Text = "HELP"
+        '
+        'ToolStripSeparator1
+        '
+        Me.ToolStripSeparator1.Alignment = System.Windows.Forms.ToolStripItemAlignment.Right
+        Me.ToolStripSeparator1.AutoSize = False
+        Me.ToolStripSeparator1.Name = "ToolStripSeparator1"
+        Me.ToolStripSeparator1.Size = New System.Drawing.Size(10, 50)
+        '
+        'tsbLogOut
+        '
+        Me.tsbLogOut.Alignment = System.Windows.Forms.ToolStripItemAlignment.Right
+        Me.tsbLogOut.AutoSize = False
+        Me.tsbLogOut.BackgroundImage = Global.STARSORG.My.Resources.Resources.logout
+        Me.tsbLogOut.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom
+        Me.tsbLogOut.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image
+        Me.tsbLogOut.ImageTransparentColor = System.Drawing.Color.Magenta
+        Me.tsbLogOut.Name = "tsbLogOut"
+        Me.tsbLogOut.Size = New System.Drawing.Size(48, 48)
+        Me.tsbLogOut.Text = "LOGOUT"
+        '
+        'ToolStripSeparator9
+        '
+        Me.ToolStripSeparator9.Alignment = System.Windows.Forms.ToolStripItemAlignment.Right
+        Me.ToolStripSeparator9.AutoSize = False
+        Me.ToolStripSeparator9.Name = "ToolStripSeparator9"
+        Me.ToolStripSeparator9.Size = New System.Drawing.Size(10, 50)
+        '
+        'tsbTutor
+        '
+        Me.tsbTutor.AutoSize = False
+        Me.tsbTutor.BackgroundImage = Global.STARSORG.My.Resources.Resources.tutors
+        Me.tsbTutor.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom
+        Me.tsbTutor.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image
+        Me.tsbTutor.ImageTransparentColor = System.Drawing.Color.Magenta
+        Me.tsbTutor.Name = "tsbTutor"
+        Me.tsbTutor.Size = New System.Drawing.Size(48, 48)
+        Me.tsbTutor.Text = "TUTOR"
+        '
+        'tsbSemester
+        '
+        Me.tsbSemester.AutoSize = False
+        Me.tsbSemester.BackgroundImage = Global.STARSORG.My.Resources.Resources.semesters
+        Me.tsbSemester.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom
+        Me.tsbSemester.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image
+        Me.tsbSemester.ImageTransparentColor = System.Drawing.Color.Magenta
+        Me.tsbSemester.Name = "tsbSemester"
+        Me.tsbSemester.Size = New System.Drawing.Size(48, 48)
+        Me.tsbSemester.Text = "SEMESTER"
+        '
+        'ToolStripSeparator2
+        '
+        Me.ToolStripSeparator2.AutoSize = False
+        Me.ToolStripSeparator2.Name = "ToolStripSeparator2"
+        Me.ToolStripSeparator2.Size = New System.Drawing.Size(10, 50)
+        '
+        'tsbCourse
+        '
+        Me.tsbCourse.AutoSize = False
+        Me.tsbCourse.BackgroundImage = Global.STARSORG.My.Resources.Resources.courses
+        Me.tsbCourse.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom
+        Me.tsbCourse.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image
+        Me.tsbCourse.ImageTransparentColor = System.Drawing.Color.Magenta
+        Me.tsbCourse.Name = "tsbCourse"
+        Me.tsbCourse.Size = New System.Drawing.Size(48, 48)
+        Me.tsbCourse.Text = "COURSE"
+        '
+        'ToolStripSeparator4
+        '
+        Me.ToolStripSeparator4.AutoSize = False
+        Me.ToolStripSeparator4.Name = "ToolStripSeparator4"
+        Me.ToolStripSeparator4.Size = New System.Drawing.Size(10, 50)
+        '
+        'ToolStripSeparator11
+        '
+        Me.ToolStripSeparator11.AutoSize = False
+        Me.ToolStripSeparator11.Name = "ToolStripSeparator11"
+        Me.ToolStripSeparator11.Size = New System.Drawing.Size(10, 50)
+        '
+        'tsbRSVP
+        '
+        Me.tsbRSVP.AutoSize = False
+        Me.tsbRSVP.BackgroundImage = Global.STARSORG.My.Resources.Resources.rsvp
+        Me.tsbRSVP.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom
+        Me.tsbRSVP.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image
+        Me.tsbRSVP.ImageTransparentColor = System.Drawing.Color.Magenta
+        Me.tsbRSVP.Name = "tsbRSVP"
+        Me.tsbRSVP.Size = New System.Drawing.Size(48, 48)
+        Me.tsbRSVP.Text = "RSVP"
+        '
+        'tsbEvent
+        '
+        Me.tsbEvent.AutoSize = False
+        Me.tsbEvent.BackgroundImage = Global.STARSORG.My.Resources.Resources.events
+        Me.tsbEvent.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom
+        Me.tsbEvent.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image
+        Me.tsbEvent.ImageTransparentColor = System.Drawing.Color.Magenta
+        Me.tsbEvent.Name = "tsbEvent"
+        Me.tsbEvent.Size = New System.Drawing.Size(48, 48)
+        Me.tsbEvent.Text = "EVENT"
+        '
+        'ToolStripSeparator5
+        '
+        Me.ToolStripSeparator5.AutoSize = False
+        Me.ToolStripSeparator5.Name = "ToolStripSeparator5"
+        Me.ToolStripSeparator5.Size = New System.Drawing.Size(10, 50)
+        '
+        'tsbRole
+        '
+        Me.tsbRole.AutoSize = False
+        Me.tsbRole.BackgroundImage = Global.STARSORG.My.Resources.Resources.roles
+        Me.tsbRole.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom
+        Me.tsbRole.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image
+        Me.tsbRole.ImageTransparentColor = System.Drawing.Color.Magenta
+        Me.tsbRole.Name = "tsbRole"
+        Me.tsbRole.Size = New System.Drawing.Size(48, 48)
+        Me.tsbRole.Text = "ROLE"
+        '
+        'ToolStripSeparator8
+        '
+        Me.ToolStripSeparator8.AutoSize = False
+        Me.ToolStripSeparator8.Name = "ToolStripSeparator8"
+        Me.ToolStripSeparator8.Size = New System.Drawing.Size(10, 50)
+        '
+        'tsbMember
+        '
+        Me.tsbMember.AutoSize = False
+        Me.tsbMember.BackgroundImage = Global.STARSORG.My.Resources.Resources.member
+        Me.tsbMember.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom
+        Me.tsbMember.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image
+        Me.tsbMember.ImageTransparentColor = System.Drawing.Color.Magenta
+        Me.tsbMember.Name = "tsbMember"
+        Me.tsbMember.Size = New System.Drawing.Size(48, 48)
+        Me.tsbMember.Text = "MEMBER"
+        '
+        'ToolStripSeparator7
+        '
+        Me.ToolStripSeparator7.AutoSize = False
+        Me.ToolStripSeparator7.Name = "ToolStripSeparator7"
+        Me.ToolStripSeparator7.Size = New System.Drawing.Size(10, 50)
+        '
+        'tsbHome
+        '
+        Me.tsbHome.AutoSize = False
+        Me.tsbHome.BackgroundImage = Global.STARSORG.My.Resources.Resources.home
+        Me.tsbHome.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom
+        Me.tsbHome.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image
+        Me.tsbHome.ImageTransparentColor = System.Drawing.Color.Magenta
+        Me.tsbHome.Name = "tsbHome"
+        Me.tsbHome.Size = New System.Drawing.Size(48, 48)
+        Me.tsbHome.Text = "HOME"
+        '
+        'ToolStripSeparator6
+        '
+        Me.ToolStripSeparator6.AutoSize = False
+        Me.ToolStripSeparator6.Name = "ToolStripSeparator6"
+        Me.ToolStripSeparator6.Size = New System.Drawing.Size(10, 50)
+        '
+        'ToolStrip1
+        '
+        Me.ToolStrip1.AutoSize = False
+        Me.ToolStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.ToolStripSeparator6, Me.tsbHome, Me.ToolStripSeparator7, Me.tsbMember, Me.ToolStripSeparator8, Me.tsbRole, Me.ToolStripSeparator5, Me.tsbEvent, Me.ToolStripSeparator3, Me.tsbRSVP, Me.ToolStripSeparator4, Me.tsbCourse, Me.ToolStripSeparator2, Me.tsbSemester, Me.tsbTutor, Me.ToolStripSeparator9, Me.tsbLogOut, Me.ToolStripSeparator1, Me.tsbHelp, Me.ToolStripSeparator11, Me.ToolStripSeparator10})
+        Me.ToolStrip1.Location = New System.Drawing.Point(0, 0)
+        Me.ToolStrip1.Name = "ToolStrip1"
+        Me.ToolStrip1.Size = New System.Drawing.Size(872, 50)
+        Me.ToolStrip1.TabIndex = 3
+        Me.ToolStrip1.Text = "ToolStrip1"
+        '
+        'ToolStripSeparator3
+        '
+        Me.ToolStripSeparator3.AutoSize = False
+        Me.ToolStripSeparator3.Name = "ToolStripSeparator3"
+        Me.ToolStripSeparator3.Size = New System.Drawing.Size(10, 50)
+        '
+        'Label1
+        '
+        Me.Label1.BackColor = System.Drawing.Color.Transparent
+        Me.Label1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 20.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(185, 406)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(503, 56)
+        Me.Label1.TabIndex = 2
+        Me.Label1.Text = "FIU STARS ORGANIZATION"
+        Me.Label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'lsvMembers
+        '
+        Me.lsvMembers.HideSelection = False
+        Me.lsvMembers.Location = New System.Drawing.Point(12, 83)
+        Me.lsvMembers.Name = "lsvMembers"
+        Me.lsvMembers.Size = New System.Drawing.Size(214, 284)
+        Me.lsvMembers.TabIndex = 4
+        Me.lsvMembers.UseCompatibleStateImageBehavior = False
+        '
+        'lblAddMember
+        '
+        Me.lblAddMember.AutoSize = True
+        Me.lblAddMember.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.lblAddMember.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblAddMember.Location = New System.Drawing.Point(277, 83)
+        Me.lblAddMember.Name = "lblAddMember"
+        Me.lblAddMember.Size = New System.Drawing.Size(128, 20)
+        Me.lblAddMember.TabIndex = 5
+        Me.lblAddMember.Text = "Add New Member"
+        '
+        'txtAddMember
+        '
+        Me.txtAddMember.Location = New System.Drawing.Point(277, 106)
+        Me.txtAddMember.Name = "txtAddMember"
+        Me.txtAddMember.Size = New System.Drawing.Size(201, 20)
+        Me.txtAddMember.TabIndex = 7
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(277, 165)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(201, 20)
+        Me.TextBox1.TabIndex = 9
+        '
+        'lblModifyMember
+        '
+        Me.lblModifyMember.AutoSize = True
+        Me.lblModifyMember.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.lblModifyMember.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblModifyMember.Location = New System.Drawing.Point(277, 142)
+        Me.lblModifyMember.Name = "lblModifyMember"
+        Me.lblModifyMember.Size = New System.Drawing.Size(113, 20)
+        Me.lblModifyMember.TabIndex = 8
+        Me.lblModifyMember.Text = "Modify Member"
+        '
+        'txtSearchMember
+        '
+        Me.txtSearchMember.Location = New System.Drawing.Point(277, 230)
+        Me.txtSearchMember.Name = "txtSearchMember"
+        Me.txtSearchMember.Size = New System.Drawing.Size(201, 20)
+        Me.txtSearchMember.TabIndex = 11
+        '
+        'lblSearchMember
+        '
+        Me.lblSearchMember.AutoSize = True
+        Me.lblSearchMember.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.lblSearchMember.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblSearchMember.Location = New System.Drawing.Point(277, 207)
+        Me.lblSearchMember.Name = "lblSearchMember"
+        Me.lblSearchMember.Size = New System.Drawing.Size(116, 20)
+        Me.lblSearchMember.TabIndex = 10
+        Me.lblSearchMember.Text = "Search Member"
+        '
+        'frmMember
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(872, 491)
+        Me.Controls.Add(Me.txtSearchMember)
+        Me.Controls.Add(Me.lblSearchMember)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.lblModifyMember)
+        Me.Controls.Add(Me.txtAddMember)
+        Me.Controls.Add(Me.lblAddMember)
+        Me.Controls.Add(Me.lsvMembers)
+        Me.Controls.Add(Me.ToolStrip1)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "frmMember"
+        Me.Text = "Member"
+        Me.ToolStrip1.ResumeLayout(False)
+        Me.ToolStrip1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents ToolStripSeparator10 As ToolStripSeparator
+    Friend WithEvents tsbHelp As ToolStripButton
+    Friend WithEvents ToolStripSeparator1 As ToolStripSeparator
+    Friend WithEvents tsbLogOut As ToolStripButton
+    Friend WithEvents ToolStripSeparator9 As ToolStripSeparator
+    Friend WithEvents tsbTutor As ToolStripButton
+    Friend WithEvents tsbSemester As ToolStripButton
+    Friend WithEvents ToolStripSeparator2 As ToolStripSeparator
+    Friend WithEvents tsbCourse As ToolStripButton
+    Friend WithEvents ToolStripSeparator4 As ToolStripSeparator
+    Friend WithEvents ToolStripSeparator11 As ToolStripSeparator
+    Friend WithEvents tsbRSVP As ToolStripButton
+    Friend WithEvents tsbEvent As ToolStripButton
+    Friend WithEvents ToolStripSeparator5 As ToolStripSeparator
+    Friend WithEvents tsbRole As ToolStripButton
+    Friend WithEvents ToolStripSeparator8 As ToolStripSeparator
+    Friend WithEvents tsbMember As ToolStripButton
+    Friend WithEvents ToolStripSeparator7 As ToolStripSeparator
+    Friend WithEvents tsbHome As ToolStripButton
+    Friend WithEvents ToolStripSeparator6 As ToolStripSeparator
+    Friend WithEvents ToolStrip1 As ToolStrip
+    Friend WithEvents ToolStripSeparator3 As ToolStripSeparator
+    Friend WithEvents Label1 As Label
+    Friend WithEvents lsvMembers As ListView
+    Friend WithEvents lblAddMember As Label
+    Friend WithEvents txtAddMember As TextBox
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents lblModifyMember As Label
+    Friend WithEvents txtSearchMember As TextBox
+    Friend WithEvents lblSearchMember As Label
+End Class
